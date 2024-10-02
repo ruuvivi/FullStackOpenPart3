5bafdafae1df6c9b7f5865cc775bafc034a4cd9c
Ruuskanen Vivi <ruuskvivi@gmail.com>
2024-10-02 13:02:59 +0300

    3.5 and 3.6 done

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -4,25 +4,27 @@ const app = express()
 let persons = [
   {
     id: "1",
-    content: "Arto Hellas",
+    name: "Arto Hellas",
     number: "040-123456"
   },
   {
     id: "2",
-    content: "Ada Lovelace",
+    name: "Ada Lovelace",
     number: "39-44-5323523"
   },
   {
     id: "3",
-    content: "Dan Abramov",
+    name: "Dan Abramov",
     number: "12-43-234345"
   },
   {
     id: "4",
-    content: "Mary Poppendieck",
+    name: "Mary Poppendieck",
     number: "39-23-6423122"
   }
 ]
+
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -30,6 +32,38 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 20000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number is missing' 
+    })
+  }
+
+  const filterNames = persons.filter(person => person.name === body.name)
+  const nameExists = filterNames.length > 0;
+  if (nameExists) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 app.get('/info', (request, response) => {
