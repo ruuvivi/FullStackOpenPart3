89a995730372feb58c5894ecbd9f282629244ed1
Ruuskanen Vivi <ruuskvivi@gmail.com>
2024-10-02 11:42:56 +0300

    3.2 done

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -32,6 +32,27 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const numOfPeople = persons.length
+  const currentTime = new Date()
+  
+  response.send(`
+    <p>Phonebook has info for ${numOfPeople} people</p>
+    <p>${currentTime}</p>
+  `)
+})
+
+/*app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    console.log('x')
+    response.status(404).end()
+  }
+}) */
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
